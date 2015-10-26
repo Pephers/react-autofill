4104f48f3b4a48664b046e08dfb11339c9d81c0a
Chiel Robben <pephers@gmail.com>
2015-10-26 18:10:25 +0100

    1.0.4

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,19 @@
 var webpack = require('webpack');
 
 module.exports = {
+    externals: {
+        react:  {
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react',
+            amd: 'react'
+        }
+    },
     entry: {
         index: './src/index.js',
     },
     output: {
-        path: __dirname + '/dist/',
+        path: __dirname + '/lib/',
         filename: '[name].js'
     },
     module: {
@@ -18,5 +26,17 @@ module.exports = {
                 loader: 'babel-loader'
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                screw_ie8: true,
+                warnings: false
+            }
+        })
+    ]
 };
